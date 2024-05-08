762d160c0f39042da372dcf98419f6e6d5d85bfb
svinpepe <2007nik.syc@gmail.com>
2024-05-08 12:41:09 +0500

    xd

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ from pyrogram import Client, filters
 
 print(pyrogram.__version__)
 app = Client("client", 1, "0")
+vivo = True
 
 
 @app.on_message(filters.me)
 def smatrphonevivo(client, msg):
-    msg.edit_text(f"{msg.text}\nСмартфон vivo")
+
+    if vivo:
+        msg.edit_text(f"{msg.text}\nСмартфон vivo")
+
+
+@app.on_message(filters.me & filters.command("stop", prefixes='/'))
+def stopvivo(client, msg):
+    global vivo
+    vivo = False
+
+
+@app.on_message(filters.me & filters.command("start", prefixes='/'))
+def startvivo(client, msg):
+    global vivo
+    vivo = True
 
 
 app.run()
